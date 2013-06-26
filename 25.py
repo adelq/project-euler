4029ebc1e412a789a90c0c288d1a013f00d76a22
Adel Qalieh <aqalieh95@gmail.com>
2013-06-26 01:57:02 -0400

    1000-digit Fibonacci number

---
+++ 25.py
@@ -0,0 +1,11 @@
+fibonaccinumbers = {0: 0, 1: 1}
+
+def fib(n):
+	if n not in fibonaccinumbers:
+		fibonaccinumbers[n] = fib(n-1) + fib(n-2)
+	return fibonaccinumbers[n]
+
+x = 0
+while len(str(fib(x))) < 1000:
+	x += 1
+print x

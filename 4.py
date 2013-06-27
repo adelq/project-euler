5c58fda9b755f9d562391e01906a6832769dc404
Adel Qalieh <aqalieh95@gmail.com>
2013-06-26 22:09:52 -0400

    Largest palindrome product

---
+++ 4.py
@@ -0,0 +1,12 @@
+def palindrome(number):
+	number = str(number)
+	return number == number[::-1]
+
+palindromelist = []
+for i in range(1000):
+	for j in range(1000):
+		product = i*j
+		if palindrome(product):
+			palindromelist.append(product)
+
+print max(palindromelist)

5d0d4e2ef1356ffec121c4c67888190f2e2bb4c5
Adel Qalieh <aqalieh95@gmail.com>
2013-06-28 22:12:05 -0400

    10001st prime

---
+++ 7.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+def prime(n):
+	for i in range(2, int(sqrt(n) + 1)):
+		if n % i == 0:
+			return False
+	return True
+
+primes = []
+i = 1
+
+while len(primes) < 10001:
+	i += 1
+	if prime(i):
+		primes.append(i)
+
+print i

3b30e83fed78fec5758fefd68277fac24bb2156b
Adel Qalieh <aqalieh95@gmail.com>
2013-06-28 22:14:58 -0400

    Summation of primes

---
+++ 10.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+def prime(n):
+	for i in range(2, int(sqrt(n) + 1)):
+		if n % i == 0:
+			return False
+	return True
+
+primes = [2]
+i = 2
+
+while primes[-1] < 2000000:
+	i += 1
+	if prime(i):
+		primes.append(i)
+
+primes.pop()
+print sum(primes)

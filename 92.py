2a3dbad79749a41ae169d7fae3653b029f59dad9
Adel Qalieh <aqalieh95@gmail.com>
2013-07-01 02:10:35 -0400

    Square digit chains

---
+++ 92.py
@@ -0,0 +1,21 @@
+def sqdigits(num):
+	num = str(num)
+	sum = 0
+	for char in num:
+		sum += int(char)**2
+	return sum
+
+def digchain(num):
+	if num == 1:
+		return 1
+	elif num == 89:
+		return 89
+	else:
+		return digchain(sqdigits(num))
+num89 = 0
+
+for i in range(1,10000000):
+	if digchain(i) == 89:
+		num89 += 1
+
+print num89

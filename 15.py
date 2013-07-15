a98563fbdbd910eb5dc040e05df75305cf2594ee
Adel Qalieh <aqalieh95@gmail.com>
2013-07-15 03:00:55 -0400

    Lattice paths

---
+++ 15.py
@@ -0,0 +1,8 @@
+from math import factorial
+
+# Combinations
+def combin(n, r):
+	return factorial(n) / (factorial(r) * factorial(n-r))
+
+# Number of paths in a m x n lattice given by combin(m+n, m)
+print combin(20 + 20, 20)
